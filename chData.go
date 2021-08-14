fa2d372a087f253b015aedab78b53561c7e1996f
et-zone <2250219329@qq.com>
2021-08-14 21:27:11 +0800

    add func

--- chData.go
+++ chData.go
@@ -179,3 +179,42 @@ func getKind(value reflect.Value) reflect.Value {
 		return reflect.ValueOf(DEFAULT_STR)
 	}
 }
+
+//dbKey to jsonKey like {"id":"aa"} ==>{"ID":"aa"}
+func ChToJsonByTagDB(src map[string]string, desStruct interface{}) (map[string]string, error) {
+	newTagdata := map[string]string{}
+	if src == nil {
+		return newTagdata, errors.New("tagData is nil,the tagData neet init")
+	}
+
+	tVal := reflect.TypeOf(desStruct)
+
+	if tVal.Kind() != reflect.Struct {
+		return newTagdata, errors.New("desStruct must struct type ")
+	}
+
+	l := tVal.NumField()
+	for i := 0; i < l; i++ {
+		jsonName := tVal.Field(i).Tag.Get("json")
+		tagName := tVal.Field(i).Tag.Get("db")
+		jsonName = strings.TrimSpace(strings.Split(jsonName, ",")[0])
+		if jsonName == "-" || tagName == "-" {
+			continue
+		}
+		if jsonName == "" {
+			jsonName = tVal.Field(i).Name
+		}
+		if tagName == "" {
+			tagName = tVal.Field(i).Name
+		}
+		if jsonName == tagName {
+			newTagdata[jsonName] = src[jsonName]
+		}
+		if src[tagName] != "" {
+			newTagdata[jsonName] = src[tagName]
+		}
+
+	}
+
+	return newTagdata, nil
+}
