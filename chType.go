5aa7ed1e0d3c3f95927d28ef4032758a53333232
et-zone <2250219329@qq.com>
2021-09-24 23:07:09 +0800

    up

--- chType.go
+++ chType.go
@@ -1,14 +1,20 @@
 package util
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
+
+const CH_TYPE = chType(0)
+
+type chType int
 
 /*
 *  replaceString,指定替换第几个字符串
 *
  */
-func ReplaceOneString(s string, old string, news string, pos int) string {
+func (t chType)ReplaceOneString(s string, old string, news string, pos int) string {
 
 	if !strings.Contains(s, old) {
 		return ""
@@ -22,12 +28,12 @@ func ReplaceOneString(s string, old string, news string, pos int) string {
 }
 
 //根据空格转为list，没有空格就没有
-func StringToList(s string) []string {
+func  (t chType)StringToList(s string) []string {
 	return strings.Fields(s)
 }
 
 //字符串是否在list里面
-func IsStringInList(list []string, sub string) bool {
+func  (t chType)IsStringInList(list []string, sub string) bool {
 	tmplist := strings.Join(list, " ")
 	if strings.Contains(tmplist, sub) {
 		return true
@@ -35,7 +41,7 @@ func IsStringInList(list []string, sub string) bool {
 	return false
 }
 
-func IsInt64InList(s []int64, i int64) bool {
+func  (t chType)IsInt64InList(s []int64, i int64) bool {
 	for _, v := range s {
 		if v == i {
 			return true
@@ -44,4 +50,21 @@ func IsInt64InList(s []int64, i int64) bool {
 
 	return false
 
+}
+
+func  (t chType)StringListToInt64(l []string)[]int64{
+	lints:=[]int64{}
+	for _,v:=range l{
+		i,_:=strconv.ParseInt(v,10,64)
+		lints=append(lints,i)
+	}
+	return lints
+}
+
+func  (t chType)Int64ListToStringList(l []int64)[]string{
+	lints:=[]string{}
+	for _,v:=range l{
+		lints=append(lints,fmt.Sprintf("%v",v))
+	}
+	return lints
 }
--- etime.go
+++ etime.go
@@ -7,17 +7,19 @@ import (
 
 const (
 	TIME_FORMAT = "2006-01-02 15:04:05"
+	TIME = tme(0)
 )
+type tme int
 
-func NewNowTimeFormat(fmt string) string {
+func (tm tme)NewNowTimeFormat(fmt string) string {
 	return time.Now().Format(fmt)
 }
 
-func TimeStringToTime(timeString string, fmt string) (time.Time, error) {
+func  (tm tme)TimeStringToTime(timeString string, fmt string) (time.Time, error) {
 	return time.Parse(fmt, timeString)
 }
 
-func TimeToString(t *time.Time, format string) string {
+func  (tm tme)TimeToString(t *time.Time, format string) string {
 	if t == nil {
 		return ""
 	}
@@ -25,14 +27,14 @@ func TimeToString(t *time.Time, format string) string {
 	return t.Format(format)
 }
 
-func TimeToStringFormat(t *time.Time, format string) string {
+func  (tm tme)TimeToStringFormat(t *time.Time, format string) string {
 	if t == nil {
 		return ""
 	}
 	return t.Format(format)
 }
 
-func TimeStringToUnix(s string, fmt string) (int64, error) {
+func  (tm tme)TimeStringToUnix(s string, fmt string) (int64, error) {
 	t, err := time.Parse(fmt, s)
 	if err != nil {
 		return 0, err
@@ -41,7 +43,7 @@ func TimeStringToUnix(s string, fmt string) (int64, error) {
 }
 
 //return like  2006-01-02
-func DateTimeToDataString(s string) (string, error) {
+func  (tm tme)DateTimeToDataString(s string) (string, error) {
 	if s == "" {
 		return "", errors.New("s not datetime")
 	}
@@ -49,7 +51,7 @@ func DateTimeToDataString(s string) (string, error) {
 }
 
 //return like  15:04:05
-func DateTimeToTimeString(s string) (string, error) {
+func  (tm tme)DateTimeToTimeString(s string) (string, error) {
 	if s == "" {
 		return "", errors.New("s not datetime")
 	}
@@ -65,7 +67,7 @@ func DateTimeToTimeString(s string) (string, error) {
 	Friday
 	Saturday
 */
-func DateTimeToWeek(s string, fmt string) (time.Weekday, error) {
+func  (tm tme)DateTimeToWeek(s string, fmt string) (time.Weekday, error) {
 	t, err := time.Parse(fmt, s)
 	if err != nil {
 		return 0, err
